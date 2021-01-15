bb7822ffbbea40811a8965a6939ffc720d35d094
Mario Rabelo <mutrano@gmail.com>
2021-01-15 17:40:03 -0300

    add SellerDaoJDBC and  DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,5 +13,7 @@ public class Program {
 		System.out.println(dep);
 		Seller seller = new Seller(21, "bob", "Bob@gmail.com",LocalDate.now(), 3000.0, dep);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 }
