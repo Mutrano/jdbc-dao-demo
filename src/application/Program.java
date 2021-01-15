91d3d9a82d231a824d2f6cb19f1c25cd4d2b9ae4
Mario Rabelo <mutrano@gmail.com>
2021-01-15 16:56:36 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department dep = new Department(1, "Books");
 		System.out.println(dep);
+		Seller seller = new Seller(21, "bob", "Bob@gmail.com",LocalDate.now(), 3000.0, dep);
+		System.out.println(seller);
 	}
 }
