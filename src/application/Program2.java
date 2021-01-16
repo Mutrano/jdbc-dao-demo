3d88788cb4f0e862b788d736b84b840d0443477f
Mario Rabelo <mutrano@gmail.com>
2021-01-16 17:59:43 -0300

    Add department delete test

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,6 +11,7 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		System.out.println("=== Test 1 : department insert ===");
 		Department dep = new Department(null,"Music");
@@ -21,11 +23,21 @@ public class Program2 {
 		departmentDao.update(dep);
 		System.out.println("Update completed");
 		
-		System.out.println("\n=== Test 3 : department findById ===");
+		System.out.println("\n=== Test 3 : department delete ===");
+		System.out.println("Enter id for delete test:");
+		int id = sc.nextInt();
+		sc.nextLine();
+		sc.close();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		
+		
+		System.out.println("\n=== Test 4 : department findById ===");
 		dep = departmentDao.findById(3);
 		System.out.println(dep);
 		
-		System.out.println("\n=== Test 4 : department findAll ===");
+		System.out.println("\n=== Test 5 : department findAll ===");
 		List<Department> departments = new ArrayList<>();
 		departments = departmentDao.findAll();
 		departments.stream().forEach(System.out::println);
